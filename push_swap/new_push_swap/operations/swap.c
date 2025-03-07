5c5e5a71f8d788ac6c7d8ec3fe480effe2db895d
razuline <razuline@icloud.com>
2025-03-07 19:09:13 +0100

    new new push_swap

--- push_swap/new_push_swap/operations/swap.c
+++ push_swap/new_push_swap/operations/swap.c
@@ -6,40 +6,47 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 13:26:09 by erazumov          #+#    #+#             */
-/*   Updated: 2025/03/07 14:52:56 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/03/07 19:07:06 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../push_swap.h"
 
-void	sa(t_elem **stack, int a)
+void	swap(t_stack *stack)
 {
-	int		tmp;
-	t_elem	*node;
+	t_node	*first;
+	t_node	*second;
 
-	node = *stack;
-	tmp = node->number;
-	node->number = node->next->number;
-	node->next->number = tmp;
-	tmp = node->index;
-	node->index = node->next->index;
-	node->next->index = tmp;
-	if (a)
-		ft_printf("sa\n");
+	if (!stack || stack->size < 2)
+		return ;
+	first = stack->head;
+	second = first->next;
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	stack->head = second;
+	if (stack->size == 2)
+		stack->tail = first;
 }
 
-void	sb(t_elem **stack, int a)
+void	sa(t_stack *a)
 {
-	int		tmp;
-	t_elem	*node;
+	swap(a);
+	ft_printf("sa\n");
+}
 
-	node = *stack;
-	tmp = node->number;
-	node->number = node->next->number;
-	node->next->number = tmp;
-	tmp = node->index;
-	node->index = node->next->index;
-	node->next->index = tmp;
-	if (a)
-		ft_printf("sb\n");
+void	sb(t_stack *b)
+{
+	swap(b);
+	ft_printf("sb\n");
+}
+
+void	ss(t_stack *a, t_stack *b)
+{
+	swap(a);
+	swap(b);
+	ft_printf("ss\n");
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 11:17:32 by erazumov          #+#    #+#             */
-/*   Updated: 2025/03/07 15:17:47 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/03/07 19:08:42 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,34 @@
 # include "libft/libft.h"
 # include "ft_printf/ft_printf.h"
 
-typedef struct	s_elem
+typedef struct	s_node
 {
-	int				number;
-	int				index;
-	struct s_elem	*prev;
-	struct s_elem	*next;
-}				t_elem;
+	int				value;
+	struct s_node	*prev;
+	struct s_node	*next;
+}				t_node;
+
+typedef struct	s_stack
+{
+	int		size;
+	t_node	*head;
+	t_node	*tail;
+}				t_stack;
+
+/* Stack's operations */
+t_stack	*init_stack(void);
+void	free_stack(t_stack *stack);
+void	push_to(t_stack *stack, int value);
+int		pop(t_stack *stack);
+void	parse_input(t_stack *stack, char **av);
 
 /* Operations */
-void	sa(t_elem **stack, int a);
-void	sb(t_elem **stack, int a);
-void	pa(t_elem **stack_a, t_elem **stack_b);
-void	pb(t_elem **stack_a, t_elem **stack_b);
+void	swap(t_stack *stack);
+void	sa(t_stack *a);
+void	sb(t_stack *b);
+void	ss(t_stack *a, t_stack *b);
 
+/* Utils */
+void	if_error(const char *str);
 
 #endif
