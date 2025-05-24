c3df6acbb34483cd702a4ace4b75a5cc03b01206
razuline <razuline@icloud.com>
2025-05-24 13:36:38 +0200

    ExamRank03 GNL in process

--- Exams/ExamRank03/get_next_line/get_next_line.h
+++ Exams/ExamRank03/get_next_line/get_next_line.h
@@ -6,7 +6,24 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 13:09:07 by erazumov          #+#    #+#             */
-/*   Updated: 2025/05/22 13:09:08 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/05/24 13:19:05 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin_free(char *s1, char *s2);
+
+char	*get_next_line(int fd);
+
+#endif
