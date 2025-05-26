71df0ad4b49b340c2cabef27f118034a9a815b68
Elena Razumovskaia <erazumov@f1r12s2.clusters.42paris.fr>
2025-05-26 12:39:07 +0200

    Level_3 print_hex DONE

--- Exams/ExamRank02/Level_3/print_hex/print_hex.c
+++ Exams/ExamRank02/Level_3/print_hex/print_hex.c
@@ -6,11 +6,16 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:46:43 by erazumov          #+#    #+#             */
-/*   Updated: 2025/05/17 17:20:56 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/05/26 12:37:36 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	print_hex(unsigned int n)
 {
@@ -18,26 +23,26 @@ void	print_hex(unsigned int n)
 
 	if (n >= 16)
 		print_hex(n / 16);
-	write(1, &hex_digits[n % 16], 1);
+	ft_putchar(hex_digits[n % 16]);
+}
+
+int	ft_atoi(char *str)
+{
+	int	result;
+
+	result = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result);
 }
 
 int	main(int ac, char **av)
 {
-	int	num;
-
-	num = ft_atoi(av[1]);
 	if (ac == 2)
-	{
-		if (num == 0)
-			write(1, "0", 1);
-		else if (num > 0)
-			print_hex(num);
-		else
-		{
-			write(1, "-", 1);
-			print_hex(-num);
-		}
-	}
-	write(1, "\n", 1);
+		print_hex(ft_atoi(av[1]));
+	ft_putchar('\n');
 	return (0);
 }
