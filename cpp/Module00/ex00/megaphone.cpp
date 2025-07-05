b6ed8f44ca8b5d24f362e0f6f7004770fa877b2f
Elena <razuline@icloud.com>
2025-07-05 15:15:12 +0200

    c++ module00 ex00 DONE

--- cpp/Module00/ex00/megaphone.cpp
+++ cpp/Module00/ex00/megaphone.cpp
@@ -6,13 +6,27 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 16:39:27 by erazumov          #+#    #+#             */
-/*   Updated: 2025/06/04 16:48:51 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/07/05 14:40:00 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cctype>
 #include <iostream>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	std::cin <<
+	int	i, j;
+
+	if (ac > 1)
+	{
+		for (i = 1; i < ac; i++)
+		{
+			j = 0;
+			for (; av[i][j] != '\0'; j++)
+				std::cout << static_cast<char>(toupper(av[i][j]));
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 }
