349d6320560425922d2db5b0479c9ccfbfb2e2a9
Elena <razuline@icloud.com>
2025-07-11 17:29:24 +0200

    exam03 broken_gnl 11.07

--- Exams/ExamRank03/broken_gnl/new_gnl.c
+++ Exams/ExamRank03/broken_gnl/new_gnl.c
@@ -6,123 +6,160 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 17:02:02 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/11 17:23:21 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/07/11 17:28:37 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <stdlib.h> // ajouté pour malloc et free
-#include <unistd.h> // ajouté pour read
-#include <fcntl.h> // ajouté pour open
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 3
+#endif
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+//cc broken_gnl.c -Wall -Wextra -Werror -D BUFFER_SIZE=3 -o gnl
+
+int	ft_strlen(char *c)
+{
+	int	i;
+
+	i = 0;
+	if (c == NULL)
+		return (0);
+	while (c[i])
+	{
+		i++;
+	}
+	return (i);
+}
 
 char	*ft_strchr(char *s, int c)
 {
-	int	i = 0;
-	if (!s) // ajouté : protection contre pointeur NULL
-		return (NULL);
-	while (s[i] != s[i] != (char)c) // modifié : ajout de la condition s[i]
-									// pour éviter dépassement
-		i++;
-	if (s[i] == (char)c) // modifié : cast pour comparer correctement
-		return (s + i);
-	else
-		return (NULL);
-}
+	int	i;
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t	i = 0; // ajouté : déclaration de l'index
-	while (i < n) // modifié : boucle correcte de 0 à n-1
+	i = 0;
+	while (s[i] && s[i] != c)
 	{
-		((char *)dest)[i] = ((char *)src)[i];
 		i++;
 	}
-		
-	return (dest);
+	if (s[i] == c)
+		return (&s[i]);
+	return (NULL);
 }
 
-size_t	ft_strlen(char *s)
+char	*ft_memmove(char *dest, char *src, int n)
 {
-	size_t	ret = 0;
-	if (!s)
-		return (NULL); // ajouté : protection contre pointeur NULL
-	while (*s)
+	int	i;
+
+	if (dest == NULL && src == NULL)
+		return (NULL);
+	if (src > dest)
 	{
-		s++;
-		ret++;
-	}
-	return (ret);
-}
-
-int	str_append_mem(char **s1, char *s2, size_t size2)
-{
-	size_t	size1 = ft_strlen(*s1);
-	if (!s1) // modifié : protection si s1 == NULL
-		return (0);
-	char	*tmp = malloc(size2 + size1 + 1);
-	if (!tmp)
-		return (0);
-	if (s1) // ajouté : copier s1 seulement si non NULL
-		ft_memcpy(tmp, *s1, size1);
-	ft_memcpy(tmp + size1, s2, size2);
-	tmp [size1 + size2] = 0;
-	free(*s1);
-	*s1 = tmp;
-	return (1);
-}
-
-int	str_append_str(char **s1, char *s2)
-{
-	return (str_append_mem(s1, s2, ft_strlen(s2)));
-}
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
-{
-	if (dest == src || n == 0) // modifié : cas où rien à faire
-		return (dest);
-	if (dest > src)
-	{
-		size_t	i = n;
-		while (i-- > 0) // modifié : copie à l'envers pour éviter l'écrasement
-			((char *)dest)[i] = ((char *)src)[i];
-	}
-	else
-	{
-		size_t	i = 0;
+		i = 0;
 		while (i < n)
 		{
-			
+			dest[i] = src[i];
+			i++;
 		}
 	}
-		return (dest);
-	size_t	i = ft_strlen((char *)src) - 1;
-	while (i >= 0)
+	else
 	{
-		
-		i--;
+		i = n - 1;
+		while (i >= 0)
+		{
+			dest[i] = src[i];
+			i--;
+		}
 	}
 	return (dest);
 }
 
-char	*get_next_line(int fd)
+char	*ft_join(char *s1, char *s2, int n)
 {
-	static char	b[BUFFER_SIZE + 1] = "";
-	char	*ret = NULL;
+	int		size1;
+	char	*retj;
 
-	char	*tmp = ft_strchr(b, '\n');
-	while (!tmp)
+	size1 = ft_strlen(s1);
+	retj = malloc(sizeof(char) * (size1 + n + 1));
+	if (retj == NULL)
 	{
-		if (!str_append_str(&ret, b))
-			return (NULL);
-		int	read_ret = read(fd, b, BUFFER_SIZE);
-		if (read_ret == -1)
-			return (NULL);
-		b[read_ret] = 0;
-	}
-	if (!str_append_mem(&ret, b, tmp - b + 1))
-	{
-		free(ret);
+		free(s1);
 		return (NULL);
 	}
-	return (ret);
+	ft_memmove(retj, s1, size1);
+	ft_memmove(retj + size1, s2, n);
+	retj[size1 + n] = '\0';
+	free(s1);
+	s1 = NULL;
+	return (retj);
+}
+
+char	*b_gnl(fd)
+{
+	static char	buf[BUFFER_SIZE + 1];
+	char		*tmp;
+	char		*rest;
+	int			nb_read;
+	int			deb;
+	int			j;
+
+	tmp = NULL;
+	rest = NULL;
+	nb_read = 0;
+	//	printf("buffer_size  = %d\n", BUFFER_SIZE);
+	while (rest == NULL)
+	{
+		if (buf[0] == '\0')
+		{
+			nb_read = read(fd, buf, BUFFER_SIZE);
+			if (nb_read == 0)
+			{
+				free(tmp);
+				return (NULL);
+			}
+		}
+		rest = ft_strchr(buf, '\n');
+		if (rest)
+		{
+			deb = 0;
+			deb = rest - buf;
+			tmp = ft_join(tmp, buf, deb + 1);
+			j = 0;
+			while (buf[j + deb + 1])
+			{
+				buf[j] = buf[j + deb + 1];
+				j++;
+			}
+			buf[j] = '\0';
+			return (tmp);
+		}
+		else
+		{
+			tmp = ft_join(tmp, buf, ft_strlen(buf));
+			buf[0] = '\0';
+		}
+	}
+	return (tmp);
+}
+
+int	main(void)
+{
+	int fd = 0;
+
+	fd = open("text1.txt", O_RDONLY);
+	if (fd < 0)
+		perror("open");
+
+	printf("line = %s", b_gnl(fd));
+	printf("line = %s", b_gnl(fd));
+	printf("line = %s", b_gnl(fd));
+	printf("line = %s", b_gnl(fd));
+	printf("line = %s", b_gnl(fd));
+	printf("line = %s", b_gnl(fd));
+	printf("line = %s", b_gnl(fd));
+
+	return (0);
 }
