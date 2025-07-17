378ec83f65cc2b29ea913b66e8a62d35cf2de546
razuline <razuline@icloud.com>
2025-07-17 13:25:43 +0200

    exam03 n_queens CORR DONE 17.07

--- Exams/ExamRank03/n_queens/n_queens.c
+++ Exams/ExamRank03/n_queens/n_queens.c
@@ -6,90 +6,76 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 13:25:45 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/15 15:19:40 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/07/17 13:24:06 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "n_queens.h"
 
-/**
- * @brief Vérifie si une position est sûre.
- * @param board Le tableau représentant l'échiquier.
- */
-int	is_safe(int row, int col, int *board)
-{
-	int	i;
+#define MAX_N 20
 
-	i = 0;
-	while (i < col)
+// Affiche une solution
+void	print_solution(int *board, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		fprintf(stdout, "%d", board[i]);
+		if (i != n - 1)
+			fprintf(stdout, " ");
+	}
+	fprintf(stdout, "\n");
+}
+
+// Vérifie si une position est sûre
+int	is_safe(int *board, int row, int col)
+{
+	for (int i = 0; i < col; i++)
 	{
 		// Vérification des menaces (ligne et diagonale) est inchangée
-		if (board[i] == row || abs(board[i] - row) == abs(i - col))
+		if (board[i] == row
+			|| (board[i] - row == i - col)
+			|| (board[i] - row == col - i))
 			return (0); // Position non sûre
-		i++;
 	}
 	return (1); // Position sûre
 }
 
-/**
- * @brief Affiche une solution.
- * @param board Le tableau représentant l'échiquier.
- */
-void	print_solution(int n, int *board)
+// Fonction récursive pour résoudre le problème
+void	solve_nqueens(int *board, int col, int n)
 {
-	int		i;
-	char	c;
-
-	i = 0;
-	while (i < n)
-	{
-		c = board[i] + '0';
-		write(1, &c, 1);
-		if (i < n - 1)
-			write(1, " ", 1);
-		i++;
-	}
-	write(1, "\n", 1);
-}
-
-/**
- * @brief Fonction récursive pour résoudre le problème.
- * @param board Le tableau représentant l'échiquier.
- */
-void	solve_n_queens(int col, int n, int *board)
-{
-	int	row;
-
 	if (col == n)
 	{
-		print_solution(n, board);
+		print_solution(board, n);
 		return ;
 	}
-	row = 0;
-	while (row < n)
+	for (int row = 0; row < n; row++)
 	{
-		if (is_safe(row, col, board))
+		if (is_safe(board, row, col))
 		{
 			board[col] = row;
 			// On passe le 'board' à l'appel récursif
-			solve_n_queens(col + 1, n, board);
+			solve_nqueens(board, col + 1, n);
 		}
-		row++;
 	}
 }
 
 int	main(int ac, char **av)
 {
-	int	n;
-	int	board[15]; // Le tableau est créé localement dans main
-
 	if (ac != 2)
-		return (1);
-
-	n = atoi(av[1]);
-	if (n <= 0 || n > 15)
+	{
+		write(1, "\n", 1);
 		return (0);
-	// On lance la récursion en passant le tableau 'board'
-	solve_n_queens(0, n, board);
+	}
+
+	int	n = atoi(av[1]);
+	if (n < 1 || n > MAX_N)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	int	board[MAX_N]; // Le tableau est créé localement dans main
+	solve_nqueens(board, 0, n);
+
 	return (0);
 }
--- Exams/ExamRank03/n_queens/n_queens.h
+++ Exams/ExamRank03/n_queens/n_queens.h
@@ -6,18 +6,19 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 13:53:05 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/15 13:57:02 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/07/17 13:04:46 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef N_QUEENS_H
 # define N_QUEENS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int		is_safe(int row, int col, int *board);
-void	print_solution(int n, int *board);
-void	solve_n_queens(int col, int n, int *board);
+void	print_solution(int *board, int n);
+int		is_safe(int *board, int row, int col);
+void	solve_nqueens(int *board, int col, int n);
 
 #endif
