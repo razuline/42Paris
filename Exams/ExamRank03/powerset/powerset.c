4b3841a17a235b8335d8caf137247266003d7061
Elena <razuline@icloud.com>
2025-07-18 10:50:37 +0200

    exam03 filter DONE 18.07

--- Exams/ExamRank03/powerset/powerset.c
+++ Exams/ExamRank03/powerset/powerset.c
@@ -6,9 +6,15 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 15:32:10 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/15 16:16:52 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/07/15 16:27:30 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/** Le terme mathématique pour "powerset" (l'ensemble de toutes les parties
+ * d'un ensemble).
+ * Concernant l'exercice que nous avons fait, le sujet était de trouver
+ * les sous-ensembles dont la somme correspond à un nombre cible.
+ */
 
 #include "powerset.h"
 
