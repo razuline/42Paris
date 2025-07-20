cbc29e1680853e762622b7b4e3ef926714e81902
Elena <razuline@icloud.com>
2025-07-20 20:04:51 +0200

    .h

--- Exams/ExamRank03/permutations/permutations_bits.h
+++ Exams/ExamRank03/permutations/permutations_bits.h
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 19:54:02 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/20 19:55:24 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/07/20 20:04:29 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-int		ft_strlen(char *str);
-void	ft_putstr(char *str);
-void	ft_swap(char *a, char *b);
-void	sort_str(char *str, int len);
-int		next_permutation(char *str, int len);
+void	perm(int *cnt, int n, int depth, char *buf);
 
 #endif
