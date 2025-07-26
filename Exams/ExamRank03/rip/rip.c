1506d5b08e2a570cae564f0896704069cf5a2220
Elena <razuline@icloud.com>
2025-07-26 18:37:59 +0200

    exam03 26.07

--- Exams/ExamRank03/rip/rip.c
+++ Exams/ExamRank03/rip/rip.c
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:30:32 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/26 13:28:41 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/07/26 17:15:56 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,19 +54,13 @@ void	get_solutions(char *buf, int n, int idx, int balance, int open,
 
 int	main(int ac, char **av)
 {
-	char	*input;
-	int		i;
-	int		n;
-	int		open;
-	int		close;
-
 	if (ac != 2)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	input = av[1];
-	n = 0;
+	char	*input = av[1];
+	int		n = 0;
 	while (input[n] != '\0')
 		n++;
 	if (n == 0)
@@ -74,15 +68,15 @@ int	main(int ac, char **av)
 
 	char	buf[n + 1];
 
-	i = 0;
+	int		i = 0;
 	while (i <= n)
 	{
 		buf[i] = input[i];
 		i++;
 	}
-	
-	open = 0;
-	close = 0;
+
+	int		open = 0;
+	int		close = 0;
 	i = 0;
 	while (i < n)
 	{
