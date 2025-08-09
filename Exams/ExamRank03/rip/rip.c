1188610459ffbecc4e19c3629486d737b059c497
Elena <razuline@icloud.com>
2025-08-09 13:31:49 +0200

    exam03 rip UPD 9.08

--- Exams/ExamRank03/rip/rip.c
+++ Exams/ExamRank03/rip/rip.c
@@ -6,50 +6,46 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:30:32 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/26 17:15:56 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/08/09 13:25:36 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rip.h"
 
-void	get_solutions(char *buf, int n, int idx, int balance, int open,
+void	get_solutions(char *buff, int n, int idx, int balance, int open,
 		int close)
 {
-	char	c;
-
 	if (idx == n)
 	{
 		if (balance == 0 && open == 0 && close == 0)
-		{
-			buf[n] = 0;
-			puts(buf);
-		}
+			puts(buff);
 		return ;
 	}
-	c = buf[idx];
+
+	char	c = buff[idx];
 	if (c == '(')
 	{
 		if (open > 0)
 		{
-			buf[idx] = ' ';
-			get_solutions(buf, n, idx + 1, balance, open - 1, close);
-			buf[idx] = '(';
+			buff[idx] = ' ';
+			get_solutions(buff, n, idx + 1, balance, open - 1, close);
+			buff[idx] = '(';
 		}
-		get_solutions(buf, n, idx + 1, balance + 1, open, close);
+		get_solutions(buff, n, idx + 1, balance + 1, open, close);
 	}
 	else if (c == ')')
 	{
 		if (close > 0)
 		{
-			buf[idx] = ' ';
-			get_solutions(buf, n, idx + 1, balance, open, close - 1);
-			buf[idx] = ')';
+			buff[idx] = ' ';
+			get_solutions(buff, n, idx + 1, balance, open, close - 1);
+			buff[idx] = ')';
 		}
 		if (balance > 0)
-			get_solutions(buf, n, idx + 1, balance - 1, open, close);
+			get_solutions(buff, n, idx + 1, balance - 1, open, close);
 	}
 	else
-		get_solutions(buf, n, idx + 1, balance, open, close);
+		get_solutions(buff, n, idx + 1, balance, open, close);
 }
 
 int	main(int ac, char **av)
@@ -59,30 +55,35 @@ int	main(int ac, char **av)
 		write(1, "\n", 1);
 		return (0);
 	}
+
 	char	*input = av[1];
 	int		n = 0;
-	while (input[n] != '\0')
+	while (input[n])
 		n++;
 	if (n == 0)
-		return (0);
-
-	char	buf[n + 1];
-
-	int		i = 0;
-	while (i <= n)
 	{
-		buf[i] = input[i];
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	char	buff[MAX_LEN];
+	int		i = 0;
+
+	while (i < n)
+	{
+		buff[i] = input[i];
 		i++;
 	}
+	buff[n] = '\0';
 
 	int		open = 0;
 	int		close = 0;
 	i = 0;
 	while (i < n)
 	{
-		if (buf[i] == '(')
+		if (buff[i] == '(')
 			open++;
-		else if (buf[i] == ')')
+		else if (buff[i] == ')')
 		{
 			if (open > 0)
 				open--;
@@ -91,6 +92,6 @@ int	main(int ac, char **av)
 		}
 		i++;
 	}
-	get_solutions(buf, n, 0, 0, open, close);
+	get_solutions(buff, n, 0, 0, open, close);
 	return (0);
 }
--- Exams/ExamRank03/rip/rip.h
+++ Exams/ExamRank03/rip/rip.h
@@ -6,17 +6,19 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:30:40 by erazumov          #+#    #+#             */
-/*   Updated: 2025/07/26 13:29:53 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/08/09 13:21:19 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RIP_H
 # define RIP_H
 
-#include <unistd.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <stdio.h>
 
-void	get_solutions(char *buf, int n, int idx, int balance, int open,
+# define MAX_LEN 4096
+
+void	get_solutions(char *buff, int n, int idx, int balance, int open,
 		int close);
 
 #endif
