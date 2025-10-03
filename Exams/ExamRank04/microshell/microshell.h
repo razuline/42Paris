e96e3832213bbc573ed128f9dc9e9530ed171a52
Elena <razuline@icloud.com>
2025-10-03 15:02:21 +0200

    exam04 DONE

--- Exams/ExamRank04/microshell/microshell.h
+++ Exams/ExamRank04/microshell/microshell.h
@@ -6,13 +6,14 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 13:24:53 by erazumov          #+#    #+#             */
-/*   Updated: 2025/09/05 14:10:30 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/10/03 14:45:14 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
+# include <stdlib.h>
 # include <string.h>
 # include <sys/wait.h>
 # include <unistd.h>
