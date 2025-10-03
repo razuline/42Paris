9dfc2abec7647076654e142cd9b6ec52c0792ead
Elena <razuline@icloud.com>
2025-10-03 15:55:14 +0200

    exam04 DONE

--- Exams/ExamRank04/microshell/microshell_vshort.c
+++ Exams/ExamRank04/microshell/microshell_vshort.c
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:09:16 by erazumov          #+#    #+#             */
-/*   Updated: 2025/10/03 15:01:59 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/10/03 15:47:57 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * Fonction d’affichage d’erreur
+ */
 void	ft_putstr_fd2(char *str, char *arg)
 {
 	while (*str)
@@ -25,45 +28,44 @@ void	ft_putstr_fd2(char *str, char *arg)
 	write(2, "\n", 1);
 }
 
-void ft_execute(char *argv[], int i, int tmp_fd, char *env[])
+/*
+ * Fonction d’exécution d’une commande
+ */
+void ft_execute(char **av, int i, int tmp_fd, char **env)
 {
-	//overwrite ; or | or NULL with NULL to use the array as input for execve.
-	//we are here in the child so it has no impact in the parent process.
-	argv[i] = NULL;
+	av[i] = NULL;
 	dup2(tmp_fd, STDIN_FILENO);
 	close(tmp_fd);
-	execve(argv[0], argv, env);
-	ft_putstr_fd2("error: cannot execute ", argv[0]);
+	execve(av[0], av, env);
+	ft_putstr_fd2("error: cannot execute ", av[0]);
 	exit(1);
 }
 
-int	main(int argc, char *argv[], char *env[])
+int	main(int ac, char **av, char **env)
 {
-	int	i;
-	int fd[2];
-	int tmp_fd;
-	(void)argc;	// is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
+	int	fd[2];
+	(void)ac;	// is needed in exam, because the exam tester compiles with -Wall -Wextra -Werror
 
-	i = 0;
-	tmp_fd = dup(STDIN_FILENO);
-	while (argv[i] && argv[i + 1]) //check if the end is reached
+	int	i = 0;
+	int	tmp_fd = dup(STDIN_FILENO);
+	while (av[i] && av[i + 1]) //check if the end is reached
 	{
-		argv = &argv[i + 1];	//the new argv start after the ; or |
+		av = &av[i + 1];	//the new argv start after the ; or |
 		i = 0;
 		//count until we have all informations to execute the next child;
-		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 			i++;
-		if (strcmp(argv[0], "cd") == 0) //cd
+		if (strcmp(av[0], "cd") == 0) //cd
 		{
 			if (i != 2)
 				ft_putstr_fd2("error: cd: bad arguments", NULL);
-			else if (chdir(argv[1]) != 0)
-				ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]	);
+			else if (chdir(av[1]) != 0)
+				ft_putstr_fd2("error: cd: cannot change directory to ", av[1]);
 		}
-		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
+		else if (i != 0 && (av[i] == NULL || strcmp(av[i], ";") == 0)) //exec in stdout
 		{
-			if ( fork() == 0)
-				ft_execute(argv, i, tmp_fd, env);
+			if (fork() == 0)
+				ft_execute(av, i, tmp_fd, env);
 			else
 			{
 				close(tmp_fd);
@@ -72,15 +74,15 @@ int	main(int argc, char *argv[], char *env[])
 				tmp_fd = dup(STDIN_FILENO);
 			}
 		}
-		else if(i != 0 && strcmp(argv[i], "|") == 0) //pipe
+		else if(i != 0 && strcmp(av[i], "|") == 0) //pipe
 		{
 			pipe(fd);
-			if ( fork() == 0)
+			if (fork() == 0)
 			{
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[0]);
 				close(fd[1]);
-				ft_execute(argv, i, tmp_fd, env);
+				ft_execute(av, i, tmp_fd, env);
 			}
 			else
 			{
