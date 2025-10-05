1ede62e5968aa1ba0e65ebe86d7ab1897f7d0b36
Elena <razuline@icloud.com>
2025-10-05 18:35:36 +0200

    CPP Module01 ex00 DONE 5.10

---
+++ cpp/Module01/ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 17:33:30 by erazumov          #+#    #+#             */
+/*   Updated: 2025/10/05 18:34:43 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Zombie.hpp"
+
+int	main(void) {
+
+	std::cout << "--- Création d'un zombie sur la Heap ---" << std::endl;
+	// On appelle newZombie ET on stocke le pointeur retourné dans heapZombie
+	Zombie *heapZombie = newZombie("Foo");
+	// Maintenant, heapZombie contient bien l'adresse du zombie "Foo"
+	heapZombie->announce();
+
+	std::cout << "--- Création d'un zombie sur la Stack ---" << std::endl;
+	randomChump("Pile");
+
+	std::cout << "--- Destruction manuelle du zombie de la Heap ---"
+			<< std::endl;
+	// On peut maintenant supprimer le zombie en toute sécurité
+	delete heapZombie;
+
+	return 0;
+}
