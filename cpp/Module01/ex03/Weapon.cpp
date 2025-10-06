2ed51ec0a2beaeb3ef308abdcddee7ea8cf80143
Elena <razuline@icloud.com>
2025-10-06 17:34:34 +0200

    CPP Module01 ex03 IN PROCESS 6.10

---
+++ cpp/Module01/ex03/Weapon.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 17:26:46 by erazumov          #+#    #+#             */
+/*   Updated: 2025/10/06 17:32:57 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type) : _type(type) { // Liste d'initialisation
+	// Rien à faire ici, le constructeur est vide
+}
+
+Weapon::~Weapon(void) {
+	// Comme ka classe ne gère pas de mémoire allouée dynamiquement (avec new),
+	// le destructeur n'a rien de spécial à faire
+}
---
+++ cpp/Module01/ex03/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 17:05:15 by erazumov          #+#    #+#             */
+/*   Updated: 2025/10/06 17:08:31 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+
+int	main(void) {
+	{
+		Weapon	club = Weapon("crude spiked club");
+		HumanA	bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon	club = Weapon("crude spiked club");
+		HumanB	jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
