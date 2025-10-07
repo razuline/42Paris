403a87e22fecd2c2b01383815d692d8ee99f1599
Elena <razuline@icloud.com>
2025-10-07 17:02:13 +0200

    cpp Module01 ex05 DONE 7.10

---
+++ cpp/Module01/ex05/Harl.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 16:08:31 by erazumov          #+#    #+#             */
+/*   Updated: 2025/10/07 16:43:59 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl
+{
+private:
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+
+public:
+	Harl(void);
+	~Harl(void);
+
+	void	complain(std::string level);
+};
+
+#endif
