d99dddf275eb285fc9e6101744e07f4712c9544c
Elena <razuline@icloud.com>
2025-10-21 16:22:43 +0200

    Exam04 UPD 21.10

--- Exams/ExamRank04/microshell/microshell.c
+++ Exams/ExamRank04/microshell/microshell.c
@@ -5,194 +5,107 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/03 13:24:42 by erazumov          #+#    #+#             */
-/*   Updated: 2025/09/05 14:09:26 by erazumov         ###   ########.fr       */
+/*   Created: 2025/10/03 14:09:16 by erazumov          #+#    #+#             */
+/*   Updated: 2025/10/21 16:21:52 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "microshell.h"
-
-//-----------------------------------------------------------------------------
-// FONCTIONS UTILITAIRES DÉDIÉES
-//-----------------------------------------------------------------------------
-
-/**
- * @brief Calcule la longueur d'une chaîne.
- */
-int	ft_strlen(char *str)
-{
-	int	i = 0;
-
-	while (str && str[i])
-		i++;
-	return (i);
-}
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * @brief Affiche un message d'erreur sur STDERR.
+ * Fonction d’affichage d’erreur
  */
-void	print_error(char *msg, char *arg)
+void	ft_putstr_fd2(char *str, char *arg)
 {
-	write(STDERR_FILENO, msg, ft_strlen(msg));
+	while (*str)
+		write(2, str++, 1);
 	if (arg)
-		write(STDERR_FILENO, arg, ft_strlen(arg));
-	write(STDERR_FILENO, "\n", 1);
+		while(*arg)
+			write(2, arg++, 1);
+	write(2, "\n", 1);
 }
 
-/**
- * @brief Affiche "error: fatal" et quitte.
+/*
+ * Fonction d’exécution d’une commande
  */
-void	fatal_error(void)
+void ft_exec(char **av, int i, int tmp_fd, char **env)
 {
-	print_error("error: fatal", NULL);
+	av[i] = NULL;
+	dup2(tmp_fd, STDIN_FILENO);
+	close(tmp_fd);
+	execve(av[0], av, env);
+	ft_putstr_fd2("error: cannot execute ", av[0]);
 	exit(1);
 }
 
-/**
- * @brief Gère la commande intégrée 'cd'.
- */
-int	handle_cd(char **args)
+int	main(int ac, char **av, char **env)
 {
-	int	i = 0;
+	(void)ac;
+	int		i = 1; // On commence à l'index 1, après le nom du programme
+	int		fd[2];
+	int		tmp_fd = dup(STDIN_FILENO); // On sauvegarde l'entrée standard
 
-	while (args[i])
-		i++;
-	if (i != 2)
+	if (ac > 1) // On vérifie qu'il y a bien des commandes à exécuter
 	{
-		print_error("error: cd: bad arguments", NULL);
-		return (1);
-	}
-	if (chdir(args[1]) == -1)
-	{
-		print_error("error: cd: cannot change directory to ", args[1]);
-		return (1);
-	}
-	return (0);
-}
-
-//-----------------------------------------------------------------------------
-// GESTION D'UNE SÉQUENCE (avec les pipes)
-//-----------------------------------------------------------------------------
-
-/**
- * @brief Logique exécutée UNIQUEMENT par le processus enfant.
- * Ne retourne jamais car elle se termine par execve ou exit.
- */
-void	child_execute(char **cmd, char **envp, int fd_in, int *fd_pipe,
-		int has_pipe)
-{
-	/* 1. Rediriger l'entrée si elle ne vient pas de stdin */
-	if (fd_in != 0)
-	{
-		if (dup2(fd_in, STDIN_FILENO) == -1)
-			fatal_error();
-		close(fd_in);
-	}
-	/* 2. Rediriger la sortie si la commande est suivie d'un pipe */
-	if (has_pipe)
-	{
-		if (dup2(fd_pipe[1], STDOUT_FILENO) == -1)
-			fatal_error();
-		close(fd_pipe[0]);
-		close(fd_pipe[1]);
-	}
-	/* 3. Exécuter la commande */
-	execve(cmd[0], cmd, envp);
-	/* Si execve échoue, le processus enfant doit s'arrêter */
-	print_error("error: cannot execute ", cmd[0]);
-	exit(1);
-}
-
-/**
- * @brief Attend la fin de TOUS les processus enfants en attente.
- */
-void wait_for_children(void)
-{
-	/* La boucle continue tant que waitpid trouve un enfant terminé.
-	** Elle s'arrête quand waitpid retourne -1 (car il n'y a plus d'enfants) */
-	while (waitpid(-1, NULL, 0) > 0);
-}
-
-void	execute_sequence(char **args, char **envp)
-{
-	int		fd_pipe[2];
-	int		fd_in = 0;
-	pid_t	pid;
-
-	/* Cas spécial pour 'cd' qui ne doit pas être forké */
-	if (args[0] && !strcmp(args[0], "cd"))
-	{
-		handle_cd(args);
-		return ;
-	}
-	/* On parcourt la séquence pour lancer chaque commande */
-	int	i = 0;
-	while (args[i])
-	{
-		/* On isole la commande actuelle en remplaçant '|' par NULL */
-		int	cmd_start = i;
-		while (args[i] && strcmp(args[i], "|"))
-			i++;
-		int	has_pipe = (args[i] && !strcmp(args[i], "|"));
-		if (has_pipe)
-			args[i] = NULL;
-		/* Création du pipe si ce n'est pas la dernière commande */
-		if (has_pipe)
+		while (av[i]) // On boucle tant qu'il y a des arguments
 		{
-			if (pipe(fd_pipe) == -1)
-				fatal_error();
-		}
-		pid = fork();
-		if (pid == -1)
-			fatal_error();
-		if (pid == 0)
-			child_execute(&args[cmd_start], envp, fd_in, fd_pipe, has_pipe);
-		else
-		{
-			/* On ferme les descripteurs au fur et à mesure */
-			if (fd_in != 0)
-				close(fd_in);
-			if (has_pipe)
+			char	**cmd_start = &av[i]; // cmd_start pointe sur le début de la commande actuelle
+			int		j = 0;
+
+			// On cherche la fin de la commande (un séparateur ou la fin du tableau)
+			while (cmd_start[j] && strcmp(cmd_start[j], ";") && strcmp(cmd_start[j], "|"))
+				j++;
+
+			if (j > 0) // On vérifie que la commande n'est pas vide (ex: ";;")
 			{
-				close(fd_pipe[1]);
-				fd_in = fd_pipe[0];
+				if (strcmp(cmd_start[0], "cd") == 0)
+				{
+					if (j != 2)
+						ft_putstr_fd2("error: cd: bad arguments", NULL);
+					else if (chdir(cmd_start[1]) != 0)
+						ft_putstr_fd2("error: cd: cannot change directory to ", cmd_start[1]);
+				}
+				// Si c'est une commande simple (terminée par ; ou la fin)
+				else if (cmd_start[j] == NULL || strcmp(cmd_start[j], ";") == 0)
+				{
+					if (fork() == 0)
+						ft_exec(cmd_start, j, tmp_fd, env);
+					else
+					{
+						close(tmp_fd);
+						while (waitpid(-1, NULL, WUNTRACED) != -1)
+							;
+						tmp_fd = dup(STDIN_FILENO);
+					}
+				}
+				// Si c'est une commande pipée
+				else if (strcmp(cmd_start[j], "|") == 0)
+				{
+					pipe(fd);
+					if (fork() == 0)
+					{
+						dup2(fd[1], STDOUT_FILENO);
+						close(fd[0]);
+						close(fd[1]);
+						ft_exec(cmd_start, j, tmp_fd, env);
+					}
+					else
+					{
+						close(fd[1]);
+						close(tmp_fd);
+						tmp_fd = fd[0];
+					}
+				}
 			}
-			if (args[i])
+			i += j; // On avance notre index principal `i` à la commande suivante
+			if (av[i]) // LA CORRECTION FINALE EST ICI
 				i++;
 		}
+		close(tmp_fd); // Fermer le dernier descripteur
 	}
-	/* L'appel à la fonction d'attente est maintenant plus simple */
-	wait_for_children();
-	if (fd_in != 0)
-		close(fd_in);
-}
-
-//-----------------------------------------------------------------------------
-// FONCTION PRINCIPALE
-//-----------------------------------------------------------------------------
-
-int	main(int ac, char **av, char **envp)
-{
-	int	i = 1;
-	int	start_seq = 1;
-
-	if (ac < 2)
-		return (0);
-
-	while (av[i])
-	{
-		if (!strcmp(av[i], ";"))
-		{
-			av[i] = NULL;
-			execute_sequence(&av[start_seq], envp);
-			av[i] = ";";
-			start_seq = i + 1;
-		}
-		i++;
-	}
-	if (av[start_seq])
-	{
-		execute_sequence(&av[start_seq], envp);
-	}
+	// On ferme le dernier descripteur de fichier avant de quitter
 	return (0);
 }
--- Exams/ExamRank04/microshell/microshell.h
+++ Exams/ExamRank04/microshell/microshell.h
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 13:24:53 by erazumov          #+#    #+#             */
-/*   Updated: 2025/10/03 14:45:14 by erazumov         ###   ########.fr       */
+/*   Updated: 2025/10/21 16:21:18 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,5 @@
 # include <string.h>
 # include <sys/wait.h>
 # include <unistd.h>
-
-int		ft_strlen(char *str);
-void	print_error(char *msg, char *arg);
-void	fatal_error(void);
-int		handle_cd(char **args);
-void	child_execute(char **cmd, char **envp, int fd_in, int *fd_pipe,
-			int has_pipe);
-void	wait_for_children(void);
-void	execute_sequence(char **args, char **envp);
 
 #endif
