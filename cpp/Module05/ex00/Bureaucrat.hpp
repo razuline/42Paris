9a0fd82cae0172041340c6064b865b8af06b8fce
Elena <razuline@icloud.com>
2026-01-02 17:53:50 +0100

    cpp05 ex00 02.01.26

--- cpp/Module05/ex00/Bureaucrat.hpp
+++ cpp/Module05/ex00/Bureaucrat.hpp
@@ -6,7 +6,20 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 13:06:18 by erazumov          #+#    #+#             */
-/*   Updated: 2026/01/02 13:06:19 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/01/02 16:54:24 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+};
+
+#endif
