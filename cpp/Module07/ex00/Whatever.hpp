f4630d43df3cc84b7aba95247680d482cb86b7f0
Elena <razuline@icloud.com>
2026-01-18 15:39:59 +0100

    CPP Module07 ex00 DONE 18.01

--- cpp/Module07/ex00/Whatever.hpp
+++ cpp/Module07/ex00/Whatever.hpp
@@ -6,7 +6,40 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 14:44:39 by erazumov          #+#    #+#             */
-/*   Updated: 2026/01/18 14:44:56 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/01/18 15:37:03 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+// Swaps the values of two given parameters.
+// Does not return anything.
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+// Compares the two values passed as parameters and returns the smallest one.
+// If they are equal, it returns the second one.
+template <typename T>
+T	min(T a, T b)
+{
+	return (a < b) ? a : b;
+}
+
+// Compares the two values passed as parameters and returns the greatest one.
+// If they are equal, it returns the second one.
+template <typename T>
+T	max(T a, T b)
+{
+	return (a > b) ? a : b;
+}
+
+#endif
