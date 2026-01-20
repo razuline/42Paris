73bc50270690f7ca0e44b3af0227484a43226463
razuline <razuline@icloud.com>
2026-01-20 12:33:44 +0100

    cpp08 ex00 IN PROCESS 20.01

---
+++ cpp/Module08/ex00/easyfind.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 11:51:13 by erazumov          #+#    #+#             */
+/*   Updated: 2026/01/20 12:30:35 by erazumov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm> // std::find(first, last, value)
+# include <exception>
+# include <iostream>
+
+/* ------------------------- CUSTOM EXCEPTION CLASS ------------------------- */
+
+class NoOccurenceException : public std::exception
+{
+public:
+	virtual const char	*what() const throw()
+	{
+		return "Error: no occurrence is found in the container!";
+	}
+};
+
+/* ---------------------------- FUNCTION TEMPLATE --------------------------- */
+
+template <typename T>
+typename T::iterator	easyfind(T &container, int value)
+{
+	typename T::iterator
+	it = std::find(container.begin(), container.end(), value);
+
+	if (it == container.end())
+	{
+		throw NoOccurenceException();
+	}
+	return it;
+}
+
+#endif
