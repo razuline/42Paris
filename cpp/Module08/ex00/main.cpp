01eda3351035e7863bc1c41d7bf01f2527f02240
razuline <razuline@icloud.com>
2026-01-20 12:51:32 +0100

    cpp08 ex00 DONE 20.01

--- cpp/Module08/ex00/main.cpp
+++ cpp/Module08/ex00/main.cpp
@@ -6,16 +6,57 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 12:31:19 by erazumov          #+#    #+#             */
-/*   Updated: 2026/01/20 12:32:14 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/01/20 12:50:00 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <list>
 
 int	main(void)
 {
+	// Test 1: Vectors
+	std::cout << "--- Test Vector ---" << std::endl;
 
+	std::vector<int>	v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+
+	try
+	{
+		std::vector<int>::iterator	it = easyfind(v, 20);
+		std::cout << "Found: " << *it << std::endl;
+
+		// Find number isn't present
+		easyfind(v, 42);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	// Test 2: List
+	std::cout << "\n--- Test List ---" << std::endl;
+
+	std::list<int>	l;
+	l.push_back(1);
+	l.push_back(2);
+	l.push_back(3);
+
+	try
+	{
+		std::list<int>::iterator	it = easyfind(l, 3);
+		std::cout << "Found: " << *it << std::endl;
+
+		easyfind(l, -5);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
 }
