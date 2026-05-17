486e49810e7a99ee27e7bbd4501082021fa23a88
Elena <razuline@icloud.com>
2026-05-17 14:38:49 +0200

    Exam05: 0_vect2 UPD 17.05

--- Exams/ExamRank05/0_vect2/vect2_exam.hpp
+++ Exams/ExamRank05/0_vect2/vect2_exam.hpp
@@ -6,47 +6,11 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 14:34:12 by erazumov          #+#    #+#             */
-/*   Updated: 2026/05/13 14:54:50 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/05/17 14:36:42 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECT2_EXAM_HPP
 # define VECT2_EXAM_HPP
-
-# include <iostream>
-
-class vect2
-{
-private:
-	int		_x;
-	int		_y;
-
-public:
-	vect2();
-	vect2(int x, int y);
-	vect2(const vect2 &copy);
-	vect2	&operator=(const vect2 &other);
-	~vect2();
-
-	vect2	&operator+=(const vect2 &other);
-	vect2	operator+(const vect2 &other) const;
-
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-	vect2	&operator=(const vect2 &other);
-}
 
 #endif
