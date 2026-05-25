74357e0f8e5d57e1e831d5c2e883b3be3270564b
Elena <razuline@icloud.com>
2026-05-25 12:43:24 +0200

    webserv: CGI.cpp IN PROGRESS 25.05

--- webserv/srcs/CGI.cpp
+++ webserv/srcs/CGI.cpp
@@ -6,8 +6,38 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/24 16:54:54 by erazumov          #+#    #+#             */
-/*   Updated: 2026/05/24 16:55:03 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/05/25 12:41:51 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CGI.hpp"
+
+/* ------------------------- ORTHODOX CANONICAL FORM ------------------------ */
+
+CGI::CGI() :
+	_env(),
+	_pid(-1)
+{
+	_pipe_in[0] = -1;
+	_pipe_in[1] = -1;
+	_pipe_out[0] = -1;
+	_pipe_out[1] = -1;
+
+	// std::cout << "Default constructor called" << std::endl;
+}
+
+CGI::~CGI()
+{
+	if (_pipe_in[0] != -1)
+		close(_pipe_in[0]);
+	if (_pipe_in[1] != -1)
+		close(_pipe_in[1]);
+
+	if (_pipe_out[0] != -1)
+		close(_pipe_out[0]);
+	if (_pipe_out[1] != -1)
+		close(_pipe_out[1]);
+	_clearEnv();
+
+	// std::cout << "Destructor called" << std::endl;
+}
