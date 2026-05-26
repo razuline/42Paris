9e925eae5b1afef7e5f62c4d20041bef12aba5f2
Elena <razuline@icloud.com>
2026-05-26 21:19:02 +0200

    webserv: main.cpp UPD 26.05

--- webserv/srcs/main.cpp
+++ webserv/srcs/main.cpp
@@ -6,15 +6,15 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 15:07:21 by erazumov          #+#    #+#             */
-/*   Updated: 2026/05/23 17:06:34 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/05/26 21:18:05 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cluster.hpp"
+
 #include <csignal>
 #include <iostream>
 
-// Global flag to control the server's main loop
 volatile sig_atomic_t	g_stop = 0;
 
 // Function to handle system signals (like Ctrl+C)
@@ -25,37 +25,33 @@ void	handle_signal(int sig)
 	std::cout << "\n[Signal received] Stopping the webserv..." << std::endl;
 }
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	// 1. ARGUMENT CHECK: Ensure the user provides at most one config file
 	if (ac > 2)
 	{
 		std::cerr << "Usage: ./webserv [configuration_file]" << std::endl;
 		return 1;
 	}
 
-	// 2. CONFIGURATION: Load the rules from the .conf file
-	// If no file is provided, use a default path
 	std::string			configPath = (ac == 2) ? av[1] : "configs/default.conf";
-	std::vector<Config>	configs = configParser.getConfigs();
-
 	std::cout << "Loading configuration: " << configPath << std::endl;
+
+	// 1. Single config instance for now (will receive vector after location parser)
+	Config				config;
 	config.parse(configPath);
 
-	// 3. SERVER INITIALISATION: Create the server instance with the loaded rules
-	Server server(config);
+	std::vector<Config>	configs;
+	configs.push_back(config);
 
-	// 4. SIGNAL SETUP: Bind the SIGINT (Ctrl+C) to the handler
-	// This is mandatory for a clean 42 project
+	// 2. Setup system signal handlers for graceful shutdown
 	signal(SIGINT, handle_signal);
 	signal(SIGQUIT, handle_signal);
 
-	// 5. LAUNCH: Setup the sockets and start the polling loop
-	Cluster.setup(configs);
-	server.run();
+	// 3. Launch Cluster multiplexer loop
+	Cluster				cluster;
+	cluster.setup(configs);
+	cluster.run();
 
-	// 6. EXIT: Once run() finishes (when g_stop == 1), the program ends here
-	std::cout << "Webserv closed successfully. See you later!" << std::endl;
-
+	std::cout << "Webserv closed successfully" << std::endl;
 	return 0;
 }
