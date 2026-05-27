c99a7fbf053d9ea55ed4370e80696514f47341e3
Elena <razuline@icloud.com>
2026-05-27 16:30:34 +0200

    webserv: Config UPD 27.05

--- webserv/includes/Config.hpp
+++ webserv/includes/Config.hpp
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 13:38:56 by erazumov          #+#    #+#             */
-/*   Updated: 2026/05/26 17:34:16 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/05/27 16:26:21 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,17 @@
 # include <iostream>
 # include <sstream>
 
+# include "Location.hpp"
+
 class Config
 {
 private:
-	int				_port;
-	std::string		_serverName;
-	std::string		_folderRoot;
-	std::string		_homePage;
-	size_t			_clientMaxBodySize;
+	int						_port;
+	std::string				_serverName;
+	std::string				_folderRoot;
+	std::string				_homePage;
+	size_t					_clientMaxBodySize;
+	std::vector<Location>	_locations;
 
 public:
 	/* --- Orthodox Canonical Form --- */
@@ -38,11 +41,12 @@ public:
 	void	parse(const std::string &filename);
 
 	/* --- Getters --- */
-	int					getPort() const;
-	const std::string	&getFolderRoot() const;
-	const std::string	&getServerName() const;
-	const std::string	&getHomePage() const;
-	size_t				getClientMaxBodySize() const;
+	int							getPort() const;
+	const std::string			&getFolderRoot() const;
+	const std::string			&getServerName() const;
+	const std::string			&getHomePage() const;
+	size_t						getClientMaxBodySize() const;
+	const std::vector<Location>	&getLocations() const;
 };
 
 #endif
--- webserv/srcs/Config.cpp
+++ webserv/srcs/Config.cpp
@@ -6,7 +6,7 @@
 /*   By: erazumov <erazumov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 13:46:06 by erazumov          #+#    #+#             */
-/*   Updated: 2026/05/26 17:39:08 by erazumov         ###   ########.fr       */
+/*   Updated: 2026/05/27 16:30:06 by erazumov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ Config::Config() :
 	_serverName(""),
 	_folderRoot(""),
 	_homePage(""),
-	_clientMaxBodySize(1048576)
+	_clientMaxBodySize(1048576),
+	_locations()
 {
 }
 
@@ -29,7 +30,8 @@ Config::Config(const Config &copy) :
 	_serverName(copy._serverName),
 	_folderRoot(copy._folderRoot),
 	_homePage(copy._homePage),
-	_clientMaxBodySize(copy._clientMaxBodySize)
+	_clientMaxBodySize(copy._clientMaxBodySize),
+	_locations(copy._locations)
 {
 }
 
@@ -38,11 +40,12 @@ Config
 {
 	if (this != &other)
 	{
-		this->_port = other._port;
-		this->_serverName = other._serverName;
-		this->_folderRoot = other._folderRoot;
-		this->_homePage = other._homePage;
-		this->_clientMaxBodySize= other._clientMaxBodySize;
+		_port = other._port;
+		_serverName = other._serverName;
+		_folderRoot = other._folderRoot;
+		_homePage = other._homePage;
+		_clientMaxBodySize = other._clientMaxBodySize;
+		_locations = other._locations;
 	}
 	return *this;
 }
@@ -102,6 +105,55 @@ Config::parse(const std::string &filename)
 			std::stringstream	ss(value);
 			ss >> _clientMaxBodySize;
 		}
+		// --- LOCATION PARSING ---
+		else if (line.find("location") == 0)
+		{
+			Location	loc;
+			size_t		pathStart = 8;
+			size_t		bracketPos = line.find("{");
+
+			std::string	rawPath = line.substr(pathStart,
+				(bracketPos != std::string::npos
+					? bracketPos - pathStart
+					: std::string::npos));
+			loc.setPath(Utils::trim(rawPath));
+
+			// Sub-loop to read the inner context of the bracket block
+			while (std::getline(file, line) && Utils::trim(line) != "}")
+			{
+				size_t	innerComment = line.find("#");
+				if (innerComment != std::string::npos)
+					line.erase(innerComment);
+
+				line = Utils::trim(line);
+				if (line.empty())
+					continue;
+
+				if (line.find("root") == 0)
+					loc.setRoot(Utils::trim(line.substr(4)));
+				else if (line.find("index") == 0)
+					loc.setIndex(Utils::trim(line.substr(5)));
+				else if (line.find("return") == 0)
+					loc.setRedirect(Utils::trim(line.substr(6)));
+				else if (line.find("upload_store") == 0)
+					loc.setUploadStore(Utils::trim(line.substr(12)));
+				else if (line.find("autoindex") == 0)
+				{
+					std::string	flag = Utils::trim(line.substr(9));
+					loc.setAutoindex(flag == "on");
+				}
+				else if (line.find("allow_methods") == 0)
+				{
+					std::vector<std::string>	methods;
+					std::stringstream			mss(Utils::trim(line.substr(13)));
+					std::string					methodToken;
+					while (mss >> methodToken)
+						methods.push_back(methodToken);
+					loc.setMethods(methods);
+				}
+			}
+			_locations.push_back(loc);
+		}
 	}
 	file.close();
 }
@@ -136,4 +188,10 @@ size_t
 Config::getClientMaxBodySize() const
 {
 	return _clientMaxBodySize;
+}
+
+const std::vector<Location>
+&Config::getLocations() const
+{
+	return _locations;
 }
