6dcc407ac7d61103db87f3ec3f0806181c84db00
Elena <razuline@icloud.com>
2026-07-12 20:48:00 +0200

    Exam06: in progress 12.07

--- Exams/ExamRank06/mini_serv.c
+++ Exams/ExamRank06/mini_serv.c
@@ -91,13 +91,14 @@ main(int ac, char **av)
 			if (!FD_ISSET(fd, &read_set))
 				continue;
 
+			int	client_fd;
 			if (fd == server_fd)
 			{
 				struct sockaddr_in	client_addr;
 				socklen_t			addr_len = sizeof(client_addr);
 
 				// Accept
-				int	client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+				client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
 				if (client_fd < 0)
 					continue;
 
@@ -108,11 +109,19 @@ main(int ac, char **av)
 					max_fd = client_fd;
 
 				sprintf(buff_write, "server: client %d just arrived\n", clients[client_fd].id);
-				send_all(client_fd, server_fd, buff_write);
+				broadcast(client_fd, server_fd, buff_write);
 			}
 			else
 			{
-
+				ssize_t	bytes_read = recv(client_fd, buff_read, buff_read - 1, 0);
+				if (bytes_read <= 0)
+				{
+					sprintf(buff_write, "server: client %d just left\n", clients[client_fd].msg);
+					broadcast(client_fd, server_fd, buff_write);
+					free(clients[fd].msg);
+					FD_CLR(fd, &master_set);
+					close(client_fd);
+				}
 			}
 		}
 	}
